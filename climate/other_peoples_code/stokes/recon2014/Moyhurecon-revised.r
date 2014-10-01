dcf350ca13ef67d8c3dc2e6cffc3b6c9daa0ba0d
unknown <rbroberg@CHAPEL03.uccs.edu>
2014-09-30 20:44:33 -0600

    opc

--- climate/other_peoples_code/stokes/recon2014/Moyhurecon-revised.r
+++ climate/other_peoples_code/stokes/recon2014/Moyhurecon-revised.r
@@ -6,8 +6,8 @@
 
 # This part from NAS
 n <- 600;
-# baseline <- n - 0:99;	# Stokes original
-baseline <- n - 100:120;
+#baseline <- n - 0:99;	# NAS original
+baseline <- 250:349;
 #baseline <- 1:600
 phi <- 0.9;
 
@@ -16,11 +16,11 @@ nrecon=300	# number of recons to run
 
 HSIndex <- function(x) # hacked to provide rescaling for recons.
 {
-	#(mean(x[baseline]) - mean(x)) / sqrt(var(x)); # orig NAS
+	(mean(x[baseline]) - mean(x)) / sqrt(var(x)); # orig NAS
 
 	# stoke scaling
-	v = (mean(x[baseline]) - mean(x))# / sqrt(var(x));
-	sign(v)/sqrt(var(x))
+	#v = (mean(x[baseline]) - mean(x))# / sqrt(var(x));
+	#sign(v)/sqrt(var(x))
 
 	# mean(x) # upside down hockey sticks
 	# mean(x[baseline]) # near zero no hockey sticks
@@ -28,19 +28,31 @@ HSIndex <- function(x) # hacked to provide rescaling for recons.
 	# mean(x) / sqrt(var(x)) # upside down hockey sticks
 }
 
-SimulatePC1 <- function(p = 50)  # Does recon as well
+CreateSimProxies <- function(p=50)
 {
-	if(T){
-		a <- matrix(NA, n, p); 
-		for (j in 1:p) {
-			b <- arima.sim(model = list(ar = phi), n);
-			a[ , j] <- b - mean(b[baseline]); # centers model on recent
-			#a[ , j] <- b - mean(b); # centers model on full span
-		}
-	}###
-	a1<<-a # once done, remove this and set T to F in calc above
-	w=recon(a1)
+	a <- matrix(NA, n, p); 
+	for (j in 1:p) {
+		b <- arima.sim(model = list(ar = phi), n);
+		a[ , j] <- b - mean(b[baseline]); # centers model on recent
+	}
+	return(a)
+}
+
+SimulatePC <- function(proxies)  # Does recon as well
+{
+	w=recon(proxies)
 	invisible(w);
+	#invisible(svd(proxies)$u[,1:npc])
+}
+
+SimulatePC1NAS <- function(p = 50)
+{
+	a <- matrix(NA, n, p);
+	for (j in 1:p) {
+		b <- arima.sim(model = list(ar = phi), n);
+		a[ , j] <- b - mean(b[baseline]);
+	}
+	invisible(svd(a)$u[,1]);
 }
 
 recon=function(a){  #L=svu$u$vec
@@ -52,10 +64,12 @@ recon=function(a){  #L=svu$u$vec
 }
 
 M=NULL
+
 a <- matrix(NA, n, nrecon);
 
 for (j in 1:ncol(a)) {
-	a[ , j] <- SimulatePC1();
+	proxies=CreateSimProxies();
+	a[ , j] <- SimulatePC(proxies);
 }
 
 b <- apply(a, 2, HSIndex);  # rescales
@@ -66,29 +80,30 @@ cav=rowMeans(c) # take average of all runs
 #graphics.off()
 #png("recon3.png",width=700)  # Change name as needed
 
-matplot(c, type = "l", xlim=c(0,600),ylim=range(c), xlab = "years", ylab = "", lty = 2,main=paste("Recon with ",npc," PC, rescaled. AR1(",phi,")",sep=''));
-lines(cav,type='l',col="black",lwd=3) # ,ylim=c(-0.1,0.1)
+matplot(c, type = "l", xlim=c(0,600),ylim=range(c), xlab = "years", ylab = "", lty = 2,main=paste(nrecon," recon with ",npc," PC, rescaled. AR1(",phi,")",sep=''));
+#matplot(c, type = "l", xlim=c(0,600),ylim=c(-0.1,0.1), xlab = "years", ylab = "", lty = 2,main=paste("Recon with ",npc," PC, rescaled. AR1(",phi,")",sep=''));
+lines(cav,type='l',col="black",lwd=3) # 
 abline(h=mean(c),col="white",lwd=1.5)
-if (nrecon<10) {legend("topleft",paste("Recon",1:nrecon),text.col=1:5)}
+if (nrecon<10) {legend("topleft",paste(nrecon,"recon",1:nrecon),text.col=1:nrecon)}
 
-'''
-if(F){  # Set to t to make AR1 model eivecs
+
+# make AR1 model eivecs
 PopulationCov <- function(n)  # NAS routine
 {
-a <- matrix(NA, n, n);
-a[] <- phi^abs(row(a) - col(a));
-for (i in 1:n)
-a[i, ] <- a[i, ] - mean(a[i, baseline]);
-for (j in 1:n)
-a[ , j] <- a[ , j] - mean(a[baseline, j]);
-invisible(a);
+	a <- matrix(NA, n, n);
+	a[] <- phi^abs(row(a) - col(a));
+	for (i in 1:n)
+		a[i, ] <- a[i, ] - mean(a[i, baseline]);
+	for (j in 1:n)
+		a[ , j] <- a[ , j] - mean(a[baseline, j]);
+	invisible(a);
 }
 e <- eigen(PopulationCov(n));
-for(i in 1:5)lines(e$vectors[,i], col = i, lwd = 2);
-#lines(e$vectors[,2], col = 2, lwd = 2);
-lines(c(0,600),c(0,0))
-legend("topleft",paste("Eivec",1:5),text.col=1:5)
+for(i in 1:npc)
+	lines(e$vectors[,i], col = i, lwd = 2);
 
-}###
-dev.off()
-'''
+	
+#lines(e$vectors[,2], col = 2, lwd = 2);
+#lines(c(0,600),c(0,0))
+#if (nrecon<10) {legend("topleft",paste("Eivec",1:nrecon),text.col=1:nrecon)"
+
