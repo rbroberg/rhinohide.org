7b8d0f97e6beb27b623c8c0b0f7a7d72faf333c2
rbroberg <ronald.broberg@mdnt.com>
2014-10-01 16:11:08 -0600

    python arima HSI NAS

--- climate/src/arima/arima_pca.py
+++ climate/src/arima/arima_pca.py
@@ -6,22 +6,23 @@ numpy.linalg.lstsq
 
 npc = 1
 nproxies = 50
-nyears = 200
+nyears = 600
 nrecon = 5
 
-ar = [.9, 0]
-ma = [1., 0.4]
+
+ar = [1, -0.9]
+ma = [1, 1]
 
 def HSIndex(x):
-	return (np.mean(x[50:150])-np.mean(x)) / np.std(x)
+	return (np.mean(x[500:600])-np.mean(x)) / np.std(x)
 
 def SimulatePC1(p=50):
 	m = np.zeros((nyears,p))
 	for j in range(p):
 		b = arma_generate_sample(ar, ma, nyears)
-		m[:,j] = b - np.mean(b[50:150])
-	u,_,_ = svd(m)
-	return(u[0:npc])
+		m[:,j] = b - np.mean(b[500:600])
+	u,s,v = svd(m,full_matrices=False)
+	return(u[:,0])
 
 a = np.zeros((nyears, nrecon))
 for j in range(nrecon):
@@ -30,52 +31,9 @@ for j in range(nrecon):
 b = [HSIndex(a[:,j]) for j in range(nrecon)]
 
 c=np.sign(b)*a
+cave=np.mean(c,axis=1)
 
 plt.plot(c)
+plt.plot(cave,color="black",lw=2)
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-# create proxy array
-proxies = np.zeros((nrecon, nproxies, nyears))
-
-# generate ar random proxies
-svds=np.zeros((nrecon,nyears))
-for i in range(nrecon):
-	for j in range(nproxies):
-		proxies[i,j,:] = arma_generate_sample(ar, ma, nyears)
-	svds[i,:]=svd(proxies[i,:,:].transpose())[0][0]
-
-# scaling factor per proxy
-scaler = (np.mean(proxies[:,:,50:150],axis=(1,2))-np.mean(proxies,axis=(1,2))) / np.std(proxies,axis=(1,2))**0.5
-
-U=[]
-for i in range(nrecon):
-	u,w,v = svd(proxies[i,:,:])
-	U.append(u)
-uarr = np.array(U) # 3,50,50
-
-c=uarr[:,:,0]*scaler
-
-
-# scaled proxies
-prox = proxies
-for i in range(nrecon):
-	for j in range(nproxies):
-		prox[i,j,:] = proxies[i,j,:]*scaler[i,j]
-
-plt.plot(prox)
-plt.show()
-
-
-# dim(a) # [1] 200   5
-# length(b) # [1] 5
-# dim(c) # [1] 200   5
